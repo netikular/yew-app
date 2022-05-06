7fc36c89ee594cfd2fc6f70500ba44a527443653
Kevin Pratt <kevin@paradem.co>
2022-05-06 11:34:39 -0600

    Configure tailwind

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: trunk serve
+css: npx tailwindcss -i ./tailwind.css -o ./build/site.css --watch
--- src/app.rs
+++ src/app.rs
@@ -3,10 +3,10 @@ use yew::prelude::*;
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
-        <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+        <main class="prose">
+            <img class="h-16 w-16" src="https://yew.rs/img/logo.png" alt="Yew logo" />
+            <h1 class="text-3xl">{ "Hello World!" }</h1>
+            <span class="text-sm">{ "from Yew with " }<i class="heart" /></span>
         </main>
     }
 }
